cfa5a3d821680479b08592996b133b2e40267b4b
Shiraz Esat <shiraz.esat@gmail.com>
2023-01-23 09:36:38 +0000

    Simplified code to support single route Not using db at moment (will use for auth token caching)

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-# node-mongo
+# Set up
 
-A node / express template with Mongo
+The following environment variables need to be set up:
 
-Server runs at http://localhost:3000
+| Variable            | Purpose                                                                                             |
+| ------------------- | --------------------------------------------------------------------------------------------------- |
+| MYID_API_SERVER     | This is the location of the MyID Core API                                                           |
+| MYID_API_USER       | This is the client identifier for connecting to the MyID Core API                                   |
+| MYID_API_SECRET     | This is the client shared secret for connecting to the MyID Core API                                |
+| NODE_EXTRA_CA_CERTS | (Optional) This needs to be set if SSL trust issues exist with the server hosting the MyID Core API |
 
-Tested with MongoDB 6.0.3 Community edition
+Install node packages with `npm install`
+
+Develop with `npm run dev`, which allows hot reloading of node.js server, and hosts a local server at http://localhost:3000
+
+Run on server with `node index.js`
+
+Note that this prototype doesn't track lifetime and re-use of the authentication token. It displays the JSON response from calling the API, and will show data for a user if a valid user GUID is provided or an error message if the user GUID doesn't match an existing record.
--- index.js
+++ index.js
@@ -1,26 +1,55 @@
 import express from "express";
-import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import routes from "./src/routes/crmRoutes";
+import fetch from "node-fetch";
 
 const app = express();
 const PORT = 3000;
 
-// mongoose connection
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/CRMdb");
+const clientId = process.env.MYID_API_USER;
+const clientSecret = process.env.MYID_API_SECRET;
+const authServer = new URL(
+  "/web.oauth2/connect/token",
+  process.env.MYID_API_SERVER
+);
+const apiServer = new URL("/rest.core/api", process.env.MYID_API_SERVER);
 
-// bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-routes(app);
+app.route("/contact/:contactId").get((req, res) => {
+  const auth = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
+  fetch(authServer, {
+    method: "POST",
+    body: "grant_type=client_credentials",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${auth}`,
+    },
+  })
+    .then((response) => response.json())
+    .catch(() => {
+      res.send(`Unable to get details for ${req.params.contactId}`);
+    })
+    .then((response) =>
+      fetch(`${apiServer}/people/${req.params.contactId}`, {
+        headers: {
+          Authorization: `Bearer ${response.access_token}`,
+        },
+      })
+    )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      res.send(JSON.stringify(response));
+    })
+    .catch((err) => {
+      res.send(`Unable to get details for ${req.params.contactId} - ${err}`);
+    });
+});
 
-// serving static files
-app.use(express.static("public"));
-
-app.get("/", (req, res) =>
-  res.send(`Node and express server is running on port ${PORT}`)
-);
+// For now, all other URLs don't show anything
+app.route("/*").get((_, res) => {
+  res.send("");
+});
 
 app.listen(PORT, () => console.log(`your server is running on port ${PORT}`));
